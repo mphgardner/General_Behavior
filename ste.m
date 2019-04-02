8af87325ae5ca82bb13bb7b24e27905dc6f0bdab
mphgardner <mphgardner@gmail.com>
2019-04-02 11:31:45 -0400

    added ste.m

---
+++ ste.m
@@ -0,0 +1,16 @@
+function [STE] = ste(V)
+%Determines the standard error of a vector
+%This determines the satandard error of a vector, should be updated for
+%matrices
+
+%If V is a vector this computes the ste along the correct dimension
+if min(size(V)) == 1
+STE = nanstd(V)/(sum(~isnan(V)))^.5;
+else
+STE = nanstd(V)./((sum(~isnan(V)))).^.5;
+end
+
+
+
+end
+
